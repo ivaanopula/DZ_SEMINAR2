9a715f71ed4b565382d069161fd14dac9e2e4ce2
Ivaanopula __ <ivaanopula@gmail.com>
2022-10-24 19:38:06 +0300

    Добавил все файлы

---
+++ Example_10/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+//456 -> 5
+
+
+Console.WriteLine("Введите от [100 до 999] число.");
+int num = int.Parse(Console.ReadLine()!);
+
+
+void res(int num)
+{
+    int firstdigit = num / 100;
+    int secondDigit = (num - firstdigit * 100) / 10;
+    Console.WriteLine("Вторая цифра числа :" + secondDigit);
+   
+}
+
+ res(num);
---
+++ Example_13/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+Console.WriteLine("Введите число.");
+int num = int.Parse(Console.ReadLine()!);
+
+void ThrirdNumber(int num)
+{
+    if (num < 99)
+    {
+        Console.WriteLine("Нет третьей цифры");
+    }
+    else
+    {
+        while (num > 1000)
+        {
+            num = num / 10;
+        }
+        Console.WriteLine(num % 10);
+    }
+
+}
+
+ThrirdNumber(num);
